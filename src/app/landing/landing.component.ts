027e782ad002e92b7454607e77e15d1eb861bdc5
AlexKaroh <steam.alexkaroh@gmail.com>
2023-07-05 22:30:43 +0400

    Rework project structure

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { LandingMainComponent } from './landing-main.component';
+import { LandingHeaderComponent } from './landing-header.component';
+import { LandingFooterComponent } from './landing-footer.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    LandingHeaderComponent,
+    LandingMainComponent,
+    LandingFooterComponent,
+  ],
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
 })
-export class LandingComponent {
-
-}
+export class LandingComponent {}

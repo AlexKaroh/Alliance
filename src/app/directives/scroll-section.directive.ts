743504e77503938064b22e427712d4aa5b414cbb
Alexey Poklad <aliaksei.poklad@softclub.by>
2023-07-17 15:02:30 +0300

    Fix styles

--- src/app/directives/scroll-section.directive.ts
+++ src/app/directives/scroll-section.directive.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @angular-eslint/use-lifecycle-interface */
 import { Directive, ElementRef, Input } from '@angular/core';
 import { ScrollManagerDirective } from './scroll-manager.directive';
 
